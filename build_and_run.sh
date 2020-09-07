c85d98d21fe5c6bdd906d5c873a02b6b6d2ddaf6
Lucas K <lucas.kluber@gmail.com>
2020-09-07 20:30:51 -0300

    Yeelight wrapper repo

---
+++ build_and_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+# clone api and client
+#git clone https://github.com/Klubas/yeelight-controller-api
+#git clone https://github.com/Klubas/yeelight-controller-client
+# docker-compose.yml default Dockerfile is Dockerfile.raspberry-pi, you may want to change that.
+docker-compose up -d
