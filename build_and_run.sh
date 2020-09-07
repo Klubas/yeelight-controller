2f0fa814f053ba1881a6b759f00a09fca8f6c864
Lucas K <lucas.kluber@gmail.com>
2020-09-07 20:32:36 -0300

    update

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash 
 # clone api and client
-#git clone https://github.com/Klubas/yeelight-controller-api
-#git clone https://github.com/Klubas/yeelight-controller-client
+git clone https://github.com/Klubas/yeelight-controller-api
+git clone https://github.com/Klubas/yeelight-controller-client
+
 # docker-compose.yml default Dockerfile is Dockerfile.raspberry-pi, you may want to change that.
+
+# run locally
 docker-compose up -d
+
+# run in balena application
+balena push <application-name>
